92882cca287bf6a86825662a069781816962fe49
doofmars <jan-preuss@gmx.de>
2015-05-11 01:16:12 +0200

    Added Player object and colors

---
+++ DiceWars/HexagonalTest/Hexagonal/Player.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+
+namespace Hexagonal
+{
+    public class Player
+    {
+        private int id;
+        private int fields;
+        private Color color;
+
+        public Player(int id, Color color)
+        {
+            this.id = id;
+            this.color = color;
+        }
+
+        public int ID
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                this.id = value;
+            }
+        }
+
+        public Color Colour
+        {
+            get
+            {
+                return color;
+            }
+            set
+            {
+                this.color = value;
+            }
+        }
+
+        public int Fields
+        {
+            get
+            {
+                return fields;
+            }
+            set
+            {
+                this.fields = value;
+            }
+        }
+
+        public void addField()
+        {
+            this.fields++;
+        }
+        
+        public void removeField()
+        {
+            if (fields > 0)
+            {
+                this.fields--;
+            }
+        }
+    }
+}
---
+++ DiceWars/HexagonalTest/Hexagonal/PlayerColors.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+
+namespace Hexagonal
+{
+    class PlayerColors
+    {
+        public readonly static Color[] colors = {Color.Red, Color.Blue, Color.Green, Color.Yellow, Color.Orange, Color.Violet, Color.SkyBlue, Color.White}; 
+    }
+}
