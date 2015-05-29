d7de9ed04c6c56bd6b1662c62e103c746e85bcc2
Marco <mhmickey89@googlemail.com>
2015-05-29 10:34:47 +0200

    added Singleton-Class for the textlabels, so that it is possible to change the number of dices and the background color after a attack was performed

---
+++ DiceWars/HexagonalTest/Hexagonal/DiceLabels.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Hexagonal
+{
+    class DiceLabels
+    {
+        private DiceLabels() { }
+        private static DiceLabels instance = null;
+        private Label[,] labels; 
+
+        public static DiceLabels GetInstance
+        {
+           get
+           {
+              if (instance == null)
+              {
+                  instance = new DiceLabels();
+              }
+              return instance;
+           }
+        }
+
+        public void addLabels(Label[,] labels)
+        {
+            this.labels = labels;
+        }
+
+        public void changeLabel(int x, int y, int dices, Color backColor)
+        {
+            labels[x, y].Text = dices.ToString();
+            labels[x, y].BackColor = backColor;
+        }
+
+        public void changeLabel(int x, int y, int dices)
+        {
+            labels[x, y].Text = dices.ToString();
+        }
+    }
+}
