1050097329485b85eafc90d597ffd4f3b0355a52
doofmars <jan-preuss@gmx.de>
2015-06-04 16:37:20 +0200

    Added method to be able to shuffle lists

--- DiceWars/HexagonalTest/Hexagonal/Math.cs
+++ DiceWars/HexagonalTest/Hexagonal/Math.cs
@@ -74,6 +74,18 @@ namespace Hexagonal
 			return ConvertToFloat(System.Math.Cos(DegreesToRadians(30)) * side);
 		}
 
+        public static void Shuffle<T>(IList<T> list)  
+        {  
+            Random rng = new Random();  
+            int n = list.Count;  
+            while (n > 1) {  
+                n--;  
+                int k = rng.Next(n + 1);  
+                T value = list[k];  
+                list[k] = list[n];  
+                list[n] = value;  
+            }  
+        }
 
 		public static bool InsidePolygon(PointF[] polygon, int N, PointF p)
 		{
